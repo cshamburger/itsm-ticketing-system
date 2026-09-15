90a36a96e33648e78cb09cf3201d36630f9ffafa
Corey Shamburger <ctshamburger81@hotmail.com>
2026-09-15 06:27:20 +0000

    Fix database initialization indentation

--- app/__init__.py
+++ app/__init__.py
@@ -5,26 +5,30 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
-    
+
     app.config['SECRET_KEY'] = 'dev-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     db.init_app(app)
     login_manager.init_app(app)
-    
+
     from .models import User
-    
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    
-   from .routes import main
+
+    from .routes import main
     app.register_blueprint(main)
 
     # Create database tables if they do not already exist
     with app.app_context():
-    db.create_all()
+        db.create_all()
+
+    return app
+
 
